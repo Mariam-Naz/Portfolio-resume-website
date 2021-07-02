259c3d98d77f3aa03913b37d0d28cc6fad061b48
Mariam <mariamnaz212@gmail.com>
2021-07-02 18:46:18 +0500

    apply background

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { Typography } from '@material-ui/core';
+import './Profile.css';
+import profile_pic from '../../assets/images/profile_pic.jpeg';
+
 
 const Profile = () => {
     return (
-        <div>
-            Profile
+        <div className='profile container_shadow'>
+            <div class='profile_name'>
+                <Typography className='name'>Maaz Shah</Typography>
+                <Typography className='title'>Web Developer</Typography>
+            </div>
+            <figure class='profile_image'>
+                <img src={profile_pic} />
+            </figure>
+            <div class='profile_information'>
+                Timeline
+                <br />
+                <button>my button</button>
+            </div>
         </div>
     )
 }
