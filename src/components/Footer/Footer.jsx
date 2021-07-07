3d5f2eb8e3ce42c2f5aba1b3269ca6df5e140fb5
Mariam <mariamnaz212@gmail.com>
2021-07-07 22:52:22 +0500

    Complete Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import './Footer.css';
+import {Typography} from '@material-ui/core';
+import ResumeData from '../../utils/resumeData'
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className="footer">
+            <div className="footer_left">
+                <Typography className="footer_name">
+                    {ResumeData.name}.
+                </Typography>
+            </div>
+            <div className="footer_right">
+                <Typography className="footer_copyright">
+                    &#169; 2021 All rights reserved.
+                </Typography>
+            </div>
         </div>
     )
 }
