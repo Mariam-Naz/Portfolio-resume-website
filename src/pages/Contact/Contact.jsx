bf4ffd75f57bed51924d31c275de2bb97786659c
Mariam <mariamnaz212@gmail.com>
2021-07-18 15:50:17 +0500

    Complete Contact page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -11,12 +11,11 @@ const useStyles = makeStyles((theme) => ({
     root: {
             display: 'flex',
             flexWrap: 'wrap',
-           
+        marginBottom: theme.spacing(5),
         },
         textField: {
             marginBottom: theme.spacing(5),
-            marginRight: theme.spacing(2),
-            width: '25ch',
+            
     },
 }));
 
@@ -24,24 +23,49 @@ const Contact = () => {
     const classes = useStyles();
     return (
         <>
-            <Grid container className='section'>
-                <Grid item className='section_title'>
-                    <span></span>
-                    <h6 className='section_title_text'>Contact Form</h6>
-                </Grid>
-                <Grid itme xs={12}>
+            <Grid container className='section' spacing={7}>
+                <Grid item xs={12} sm={12} md={6} lg={6} >
                     <Grid container>
-                        <Grid item xs={12} sm={12} md={6} lg={6} className='about_body top_30'>
-                            <form className={classes.root} noValidate autoComplete="off">
-                                <TextField className={classes.textField} id="standard-basic" placeholder='Name'/>
-                                <TextField className={classes.textField} id="standard-basic" placeholder='Email' />
-                                <TextField id="standard-basic" multiline placeholder='Message'
-                                    rows={5} fullWidth/>
-                            </form>
-                            <CustomBtn text='Submit' />
+                        <Grid item className='section_title'>
+                            <span></span>
+                            <h6 className='section_title_text'>Contact Form</h6>
+                        </Grid>
+                        <Grid item xs={12}>
+                                <form className='row contact-form top_30'>
+                                    <Grid item xs={12} sm={6}>
+                                        <TextField placeholder='Name' fullWidth/>
+                                    </Grid>
+                                    <Grid item xs={12} sm={6}>
+                                        <TextField placeholder='Email' fullWidth/>
+                                    </Grid>
+                                <Grid item xs={12} sm={12} md={12} lg={12} className='margin-y'>
+                                        <TextField multiline placeholder='Message'
+                                            rows={5} fullWidth/>
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <CustomBtn text='Submit' />
+                                    </Grid>
+                                </form>
+                        </Grid>
                     </Grid>
                 </Grid>
-             
+                <Grid item xs>
+                    <Grid container>
+                        <Grid item className='section_title'>
+                            <span></span>
+                            <h6 className='section_title_text'>Contact Information</h6>
+                        </Grid>
+                        <Grid item xs={12} className='contact-info top_30'>
+                            <Typography className='contact-info-text'> <span>Address: </span>{ResumeData.address}</Typography>
+                            <Typography className='contact-info-text'> <span>Phone: </span>{ResumeData.phone}</Typography>
+                            <Typography className='contact-info-text'> <span>Email: </span>{ResumeData.email}</Typography>
+                            <div className='social-icons'>
+                            {Object.keys(ResumeData.socials).map(key => (
+                                <a href={ResumeData.socials[key].link}>{ResumeData.socials[key].icon}</a>
+                            ))}
+                            </div>
+                        </Grid>
+                    </Grid>
                 </Grid>
             </Grid>
         </>
